4a1e391cbb2d8fe97515f6cd93b7690cafd86845
Alfred Ababio <aababio75@gmail.com>
2014-05-08 00:14:26 -0400

    Added banner image and styled it responsively

--- javascripts/alfredAddsSomeCode.js
+++ javascripts/alfredAddsSomeCode.js
@@ -7,11 +7,8 @@ $(function() {
 
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			console.log("Location.pathname: " + location.pathname);
 			var target = $(this.hash);
-			console.log("target: " + target.length);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			console.log("target after ternary: " + target.offset().top);
 			if (target.length) {
 				$('html,body').animate({
 					scrollTop: (target.offset().top - 70) // 70 is how big the navbar is
